d15a31d07ea05d8b2c556486a5fe03d737a269a9
Gonzalo Arroyo <87682632+Gonzalo1013@users.noreply.github.com>
2024-11-11 19:41:10 -0300

    otro commit

--- database/config.js
+++ database/config.js
@@ -7,9 +7,7 @@ const dbConnection = async() => {
     try{
 
         await mongoose.connect( process.env.DB_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
+           
           } );
 
         console.log('DB online');
